d6c34959f96be82236680ff2edbd60fb65d52264
Sarah Allen <sarah@ultrasaurus.com>
2015-12-31 13:10:45 -0800

    install pods

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.1'
+
+xcodeproj 'ParsePlayground'
+
+use_frameworks!
+
+target :ParsePlayground, :exclusive => true do
+  pod 'Parse'
+end
---
+++ Pods/Target Support Files/Pods-ParsePlayground/Pods-ParsePlayground-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParsePlayground : NSObject
+@end
+@implementation PodsDummy_Pods_ParsePlayground
+@end
